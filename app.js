eed3cb9c8d9bf5beb7145e4ee1d5fc2e7cc73b2e
castillomiguel25 <miguelangelcastillo947@gmail.com>
2023-11-18 10:12:09 -0400

    red

--- app.js
+++ app.js
@@ -91,3 +91,12 @@ spaceBetween: 30,
 },
 },
 });
+
+$(document).ready(function () {
+    $('.post-img').click(function () {
+      // Cuando se hace clic en una imagen
+      var imgSrc = $(this).attr('src'); // Obtiene la ruta de la imagen clicada
+      $('#imagenGrande').attr('src', imgSrc); // Asigna la ruta al modal
+      $('#imagenModal').modal('show'); // Muestra el modal con la imagen en grande
+    });
+  });
